652dee2a0af988a7393f522b953cb87058ba25cc
Mr. Richardson <mr.richardson@outlook.de>
2026-05-28 22:51:13 +0200

    chore: rustfmt

--- src/ui/right.rs
+++ src/ui/right.rs
@@ -1,37 +1,17 @@
 use macroquad::color::Color;
-use macroquad::input::{is_mouse_button_down, is_mouse_button_pressed, mouse_position};
 use macroquad::prelude::{draw_rectangle, screen_height, screen_width};
 
-pub struct TempInfo {
-    pub resizing: bool,
-}
 pub struct Right {
-    pub width: f32,
     color: Color,
-    pub temp_info: TempInfo,
+    width: f32,
 }
 
 impl Right {
     pub(crate) fn new(color: Color, width: f32) -> Self {
-        Right {
-            color,
-            width,
-            temp_info: TempInfo { resizing: false },
-        }
+        Right { color, width }
     }
 
     pub fn render(&self) {
-        draw_rectangle(screen_width() - self.width, 0.0, self.width, screen_height(), self.color);
-    }
-
-    pub fn resize_check(&mut self, tolerance: f32) {
-        if (mouse_position().0 - screen_width() + self.width).abs() <= tolerance && is_mouse_button_pressed(macroquad::input::MouseButton::Left) {
-            self.width = screen_width() - mouse_position().0;
-            self.temp_info.resizing = true;
-        } else if self.temp_info.resizing && is_mouse_button_down(macroquad::input::MouseButton::Left) {
-            self.width = screen_width() - mouse_position().0
-        } else {
-            self.temp_info.resizing = false
-        }
+        draw_rectangle(screen_width() - self.width, 0.0, screen_width(), screen_height(), self.color);
     }
 }
