98b8944a5d03f7002d6e8bf3ea788ac70020ffb6
Mr. Richardson <mr.richardson@outlook.de>
2026-05-30 12:29:11 +0200

    feat(ui::right): wip resizing

--- src/ui/right.rs
+++ src/ui/right.rs
@@ -1,17 +1,39 @@
 use macroquad::color::Color;
+use macroquad::input::{is_mouse_button_down, is_mouse_button_pressed, mouse_position};
 use macroquad::prelude::{draw_rectangle, screen_height, screen_width};
+use miniquad::window::set_mouse_cursor;
 
+struct TempInfo {
+    resizing: bool,
+}
 pub struct Right {
     color: Color,
-    width: f32,
+    pub width: f32,
+    temp_info: TempInfo,
 }
 
 impl Right {
     pub(crate) fn new(color: Color, width: f32) -> Self {
-        Right { color, width }
+        Right {
+            color,
+            width,
+            temp_info: TempInfo { resizing: false },
+        }
     }
 
     pub fn render(&self) {
-        draw_rectangle(screen_width() - self.width, 0.0, screen_width(), screen_height(), self.color);
+        draw_rectangle(screen_width() - self.width, 0.0, self.width, screen_height(), self.color);
+    }
+
+    pub fn resize_check(&mut self, tolerance: f32) {
+        if ((mouse_position().0 - screen_width() + self.width).abs() <= tolerance && is_mouse_button_pressed(macroquad::input::MouseButton::Left)) {
+            self.width = screen_width() - mouse_position().0;
+            self.temp_info.resizing = true;
+        } else if self.temp_info.resizing == true && is_mouse_button_down(macroquad::input::MouseButton::Left) {
+            self.width = screen_width() - mouse_position().0
+        } else {
+            self.temp_info.resizing = false
+        }
+        set_mouse_cursor(miniquad::CursorIcon::EWResize)
     }
 }
