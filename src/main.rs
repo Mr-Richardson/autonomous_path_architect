30ec7b682b867401042a2a5ce0eb0d4f7253d8f4
Mr. Richardson <mr.richardson@outlook.de>
2026-06-05 15:56:50 +0200

    feat(ui): mouse cursor feedback for resizing and left panel resize

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod load_texture_safe;
+pub mod set_mouse_cursor;
 mod ui;
 
 use crate::ui::Manager;
@@ -19,7 +20,7 @@ async fn main() {
     let mut ui: Manager = Manager::new(vec!["straight".to_string(), "turn".to_string(), "arc".to_string(), "drive".to_string()], Color::new(0.1, 0.1, 0.1, 1.0));
 
     loop {
-        ui.resize_check(5.0);
+        ui.resize_check(4.0);
         ui.render();
         next_frame().await;
     }
