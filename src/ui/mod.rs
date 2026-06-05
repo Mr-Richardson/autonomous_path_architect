d1084ffd3ebbffdeebad891e44e611de4f584c80
Mr. Richardson <mr.richardson@outlook.de>
2026-06-05 15:46:27 +0200

    refactor(ui): move component initialization and asset loading into Manager and include assets in binary

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,4 +1,11 @@
+use crate::load_texture_safe::load_texture_safe;
+use macroquad::color::Color;
+use macroquad::input::{is_key_pressed, mouse_position};
+use macroquad::prelude::{get_default_font, load_ttf_font_from_bytes};
 use macroquad::window::screen_width;
+
+const FONT_DATA: &[u8] = include_bytes!("../../assets/font/Lexend-VariableFont_wght.ttf");
+const FIELD_TEXTURE_BYTES: &[u8] = include_bytes!("../../assets/textures/field.png");
 
 pub mod left;
 pub mod middle;
@@ -11,8 +18,18 @@ pub(crate) struct Manager {
 }
 
 impl Manager {
-    pub(crate) fn new(left: left::Left, middle: middle::Middle, right: right::Right) -> Self {
-        Manager { left, middle, right }
+    pub(crate) fn new(drive_method_names: Vec<String>, color: Color) -> Self {
+        let font = load_ttf_font_from_bytes(FONT_DATA).unwrap_or_else(|e| {
+            eprintln!("Font failed to decode: {}. Use default font instead.", e);
+            get_default_font()
+        });
+        Manager {
+            left: left::Left::new(drive_method_names, 150.0, color, font),
+            middle: middle::Middle::new(vec![], load_texture_safe(FIELD_TEXTURE_BYTES)),
+            right: right::Right::new(color, 300.0),
+            cursor: crate::set_mouse_cursor::MouseCursor { state: miniquad::CursorIcon::Default },
+            temp_info: TempInfo { is_fullscreen: false },
+        }
     }
 
     pub(crate) fn render(&mut self) {
