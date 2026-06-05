a16591eb35154099e87f2c4b4c00e2148cb82e3d
Mr. Richardson <mr.richardson@outlook.de>
2026-06-05 15:46:27 +0200

    refactor(ui): move component initialization and asset loading into Manager and include assets in binary

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 mod load_texture_safe;
 mod ui;
 
-use crate::load_texture_safe::load_texture_safe;
 use crate::ui::Manager;
-use crate::ui::left::Left;
-use crate::ui::middle::Middle;
-use crate::ui::right::Right;
 use macroquad::color::Color;
-use macroquad::prelude::load_ttf_font;
-use macroquad::text::get_default_font;
-use macroquad::window::{Conf, next_frame};
+use macroquad::window::{next_frame, Conf};
 
 fn window_conf() -> Conf {
     Conf {
@@ -22,19 +16,8 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let font = match load_ttf_font("assets/font/Lexend-VariableFont_wght.ttf").await {
-        Ok(loaded_font) => loaded_font,
-        Err(e) => {
-            eprintln!("Font failed to load: {}. Use default font instead.", e);
-            get_default_font()
-        }
-    };
+    let mut ui: Manager = Manager::new(vec!["straight".to_string(), "turn".to_string(), "arc".to_string(), "drive".to_string()], Color::new(0.1, 0.1, 0.1, 1.0));
 
-    let mut ui: Manager = Manager::new(
-        Left::new(vec!["straight".to_string(), "turn".to_string(), "arc".to_string(), "drive".to_string()], 150.0, Color::new(0.1, 0.1, 0.1, 1.0), font),
-        Middle::new(vec![], load_texture_safe("assets/textures/field.png").await),
-        Right::new(Color::new(0.1, 0.1, 0.1, 1.0), 300.0),
-    );
     loop {
         ui.resize_check(5.0);
         ui.render();
