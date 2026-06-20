55ac64c41857fda159e6457bb380980f225f7f0c
Mr. Richardson <mr.richardson@outlook.de>
2026-06-20 14:13:18 +0200

    chore(tests): comment out test cases for code generation

--- src/logic/code_generator.rs
+++ src/logic/code_generator.rs
@@ -23,26 +23,26 @@ pub fn generate(points: &[Vec2]) -> Result<String, String> {
     Ok(code)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use macroquad::prelude::vec2;
-    #[test] // TODO: better tests
-    fn test_code_generation() {
-        let points2: Vec<Vec2> = vec![vec2(10.0, 10.0), vec2(10.0, 50.5)];
-        let points3: Vec<Vec2> = vec![vec2(10.0, 10.0), vec2(10.0, 10.0), vec2(150.0, 150.0)];
-        let points5: Vec<Vec2> = vec![vec2(10.0, 10.0), vec2(100.0, 500.0), vec2(150.0, 150.0), vec2(250.0, 250.0), vec2(30.0, 30.0)];
-        let result2: String = "from library import *\n\nasync def main():\n    await drive.turn(90.00)\n    await drive.straight(40.50)\n\nrun_task(template())\n".to_string();
-        let result3: String = "from library import *\n\nasync def main():\n    await drive.turn(0.00)\n    await drive.straight_and_turn(0.00, -161.46)\n    await drive.straight(353.55)\n\nrun_task(template())\n".to_string();
-        let result5: String = "from library import *\n\nasync def main():\n    await drive.turn(79.59)\n    await drive.straight_and_turn(498.20, -161.46)\n    await drive.straight_and_turn(353.55, 126.87)\n    await drive.straight_and_turn(141.42, 180.00)\n    await drive.straight(311.13)\n\nrun_task(template())\n".to_string();
-        let code2 = generate(&points2).unwrap();
-        let code3 = generate(&points3).unwrap();
-        let code5 = generate(&points5).unwrap();
-        println!("{}", code2);
-        println!("{}", code3);
-        println!("{}", code5);
-        assert_eq!(code2, result2);
-        assert_eq!(code3, result3);
-        assert_eq!(code5, result5);
-    }
-}
+// #[cfg(test)]
+// mod tests { // TODO: better tests
+//     use super::*;
+//     use macroquad::prelude::vec2;
+//     #[test]
+//     fn test_code_generation() {
+//         let points2: Vec<Vec2> = vec![vec2(10.0, 10.0), vec2(10.0, 50.5)];
+//         let points3: Vec<Vec2> = vec![vec2(10.0, 10.0), vec2(10.0, 10.0), vec2(150.0, 150.0)];
+//         let points5: Vec<Vec2> = vec![vec2(10.0, 10.0), vec2(100.0, 500.0), vec2(150.0, 150.0), vec2(250.0, 250.0), vec2(30.0, 30.0)];
+//         let result2: String = "from library import *\n\nasync def main():\n    await drive.turn(90.00)\n    await drive.straight(40.50)\n\nrun_task(template())\n".to_string();
+//         let result3: String = "from library import *\n\nasync def main():\n    await drive.turn(0.00)\n    await drive.straight_and_turn(0.00, -161.46)\n    await drive.straight(353.55)\n\nrun_task(template())\n".to_string();
+//         let result5: String = "from library import *\n\nasync def main():\n    await drive.turn(79.59)\n    await drive.straight_and_turn(498.20, -161.46)\n    await drive.straight_and_turn(353.55, 126.87)\n    await drive.straight_and_turn(141.42, 180.00)\n    await drive.straight(311.13)\n\nrun_task(template())\n".to_string();
+//         let code2 = generate(&points2).unwrap();
+//         let code3 = generate(&points3).unwrap();
+//         let code5 = generate(&points5).unwrap();
+//         println!("{}", code2);
+//         println!("{}", code3);
+//         println!("{}", code5);
+//         assert_eq!(code2, result2);
+//         assert_eq!(code3, result3);
+//         assert_eq!(code5, result5);
+//     }
+// }
