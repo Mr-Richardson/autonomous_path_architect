fffc30a428d9fec4db09fae874d091c704ecc8dd
Felix Zotz <felix.zotz@gmail.com>
2026-08-11 09:57:31 +0200

    feat: add webview panel and placeholder icon

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,11 @@ import * as vscode from "vscode";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  console.log(
-    'Congratulations, the extension "autonomous-path-architect" is now active!',
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      "apaSidebarView",
+      new ApaWebviewViewProvider(context),
+    ),
   );
 
   // Now provide the implementation of the command with registerCommand
@@ -20,6 +23,29 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(disposable);
+}
+
+class ApaWebviewViewProvider implements vscode.WebviewViewProvider {
+  constructor(private readonly context: vscode.ExtensionContext) {}
+
+  resolveWebviewView(webviewView: vscode.WebviewView) {
+    webviewView.webview.options = {
+      enableScripts: true,
+    };
+
+    webviewView.webview.html = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <body>
+        <h1 id="grammatik">Grammatik</h1>
+<li>Durchgängig <strong>Imperfekt</strong> (Präteritum) nutzen.</li>
+<li>Weitestgehend im <strong>Passiv</strong> berichten.</li>
+<li><strong>Präzise</strong> Begriffe und Beschreibungen anstreben.</li>
+<li>Mit <strong>Stichpunkten</strong> strukturieren, Fließtext meiden.</li>
+      </body>
+      </html>
+    `;
+  }
 }
 
 // This method is called when your extension is deactivated
